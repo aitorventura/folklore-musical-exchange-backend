da12d69faf3e99c34fe181b496458b7d80bf28d3
GuillermoHRT <guillermohrt@gmail.com>
2020-04-25 12:30:32 +0200

    WIP

--- src/musicalgroup/musicalgroup.database.ts
+++ src/musicalgroup/musicalgroup.database.ts
@@ -26,6 +26,15 @@ export class MusicalGroupDataBaseConnection extends UserDataBaseConnection {
     return result;
   }
 
+  async getOthersMusicalGroups(id: number) {
+    const result = await this.knex
+      .select('*')
+      .from('MGroup')
+      .innerJoin('User', 'MGroup.id', 'User.id')
+      .where(this.knex.raw(`MGroup.id != ${id}`));
+    return result;
+  }
+
   async addNewMusicalGroup(musicalgroupDto: MusicalGroupDto) {
     /*
     SALIDA 0: Todo funciona correctamente
@@ -34,14 +43,15 @@ export class MusicalGroupDataBaseConnection extends UserDataBaseConnection {
     SALIDA 3: Error al crear el usuario
     SALIDA 4: Error al crear la agrupación
     */
-    musicalgroupDto.image = null;
+    //musicalgroupDto.image = null;
+    console.log("musicalgroupDto.image: ", musicalgroupDto.image);
     musicalgroupDto.role = 'MGROUP';
 
     const isAdded = await this.addNewUser(musicalgroupDto);
-    console.log('Resultado isadded: ' + isAdded);
+    //console.log('Resultado isadded: ' + isAdded);
     if (isAdded != 0) {
       //Si ha habido un problema devuelvo el código
-      console.log('Return: ' + isAdded);
+      //console.log('Return: ' + isAdded);
       return isAdded;
     }
 
@@ -66,13 +76,12 @@ export class MusicalGroupDataBaseConnection extends UserDataBaseConnection {
     SALIDA 3: Error al modificar el usuario
     SALIDA 4: Error al modificar la agrupación
     */
-    musicalGroupDto.image = null;
-    musicalGroupDto.role = 'MGROUP';
 
+    musicalGroupDto.role = 'MGROUP';
     const updated = await this.updateUser(musicalGroupDto);
-    console.log('Resultado: ' + updated);
+    //console.log('Resultado: ' + updated);
     if (updated != 0) {
-      console.log('Resultado: ' + updated);
+      //  console.log('Resultado: ' + updated);
       //Si ha habido un problema devuelvo el código
       return updated;
     }
@@ -88,7 +97,7 @@ export class MusicalGroupDataBaseConnection extends UserDataBaseConnection {
         return 0;
       }
     } catch (error) {
-      console.log('Error update mgroup');
+      //  console.log('Error update mgroup');
       return 4;
     }
   }
@@ -96,9 +105,7 @@ export class MusicalGroupDataBaseConnection extends UserDataBaseConnection {
   async deleteMusicalGroup(musicalgroupId: number) {
     try {
       if ((await this.getMusicalExchange(musicalgroupId)).length > 0) {
-        console.log(
-          'No se puede eliminar porque tiene un intercambio pendiente',
-        );
+        console.log('No se puede eliminar porque tiene un intercambio pendiente');
         //TODO: Tiene que saltar una excepción para mostrársela al usuario
         return false;
       }
@@ -107,7 +114,7 @@ export class MusicalGroupDataBaseConnection extends UserDataBaseConnection {
 
       return true;
     } catch (error) {
-      console.log('No se puede porque tiene intercambios');
+      //  console.log('No se puede porque tiene intercambios');
       return false;
     }
   }
--- src/user/user.database.ts
+++ src/user/user.database.ts
@@ -8,44 +8,66 @@ export class UserDataBaseConnection extends DataBaseConnection {
   }
 
   async addNewUser(userDto: UserDto) {
+    console.log("user.database.addNewUser, comprobar si existe el email")
     try {
-      let query = `SELECT * FROM User WHERE email='${userDto.email}'`;
-      console.log('query: ' + query);
-      const result = await this.knex.raw(query);
-      console.log('result[0]: ' + result[0].length);
-      console.log('result: ' + result.length);
-      if (result[0].length > 0) {
-        console.log('addUser res: return 1');
+      const result = await this.knex.select()
+        .from("User")
+        .where({ 'email': `${userDto.email}` });
+      //let query = `SELECT * FROM User WHERE email='${userDto.email}'`;
+      //console.log('Query: ' + query);
+      //const result = await this.knex.raw(query);
+
+      //console.log('result[0]: ' + result[0].length);
+      console.log('Total resultados con email ' + userDto.email + ': ' + result.length);
+      if (result.length > 0) {
+        //if (result[0] != null && result[0].length > 0) {
+        //  console.log('addUser res: return 1');
         return 1;
       }
     } catch (error) {
-      console.log('Error con el select email');
+      //console.log('Error con el select email');
       return 3;
     }
 
+    console.log("user.database.addNewUser, comprobar si existe el username")
     try {
-      let query = `SELECT * FROM User WHERE username='${userDto.username}'`;
-      console.log('query: ' + query);
-      const result = await this.knex.raw(query);
-      console.log('result[0]: ' + result[0].length);
-      console.log('result: ' + result.length);
-      if (result[0].length > 0) {
-        console.log('addUser res: return 2');
+      const result = await this.knex.select()
+        .from("User")
+        .where({ "username": `${userDto.username}` });
+      //let query = `SELECT * FROM User WHERE username='${userDto.username}'`;
+      //console.log('query: ' + query);
+      //const result = await this.knex.raw(query);
+      //console.log('result[0]: ' + result[0].length);
+      //console.log('result: ' + result.length);
+      console.log('Total resultados con username ' + userDto.username + ': ' + result.length);
+      if (result.length > 0) {
+        //if (result[0].length > 0) {
+        //  console.log('addUser res: return 2');
         return 2;
       }
     } catch (error) {
-      console.log('Error al ver si ya existe ese usuario');
+      //console.log('Error al ver si ya existe ese usuario');
       return 3;
     }
-
+    console.log("user.database.addNewUser, inserto en la tabla user")
     try {
-      let query = `INSERT INTO User(role, email, username, password, city, image)
-                VALUES ( '${userDto.role}' ,  '${userDto.email}' , '${userDto.username}', AES_ENCRYPT('${userDto.password}', 'fme') , '${userDto.city}', ${userDto.image})`;
-      console.log('query: ' + query);
-      const result = await this.knex.raw(query);
+      /*await this.knex("User").insert({
+        "username": userDto.username,
+        "email": userDto.email,
+        "password": AES_ENCRYPT(userDto.password, "fme"),
+        "role": userDto.role,
+        "city": userDto.city,
+        "image": userDto.image
+      }).then(() => {})*/
+      //const image = userDto.image.split(",");
+      //userDto.image = image[1];
+      let query = `INSERT INTO User(role, email, username, password, city, image) VALUES ( '${userDto.role}' ,  '${userDto.email}' , '${userDto.username}', AES_ENCRYPT('${userDto.password}', 'fme') , '${userDto.city}', '${userDto.image}')`;
+      console.log('user.database.addNewUser, Query: ' + query);
+      await this.knex.raw(query);
       return 0;
     } catch (error) {
-      console.log('Error al crear el usuario');
+      console.log(error);
+      //console.log('Error al crear el usuario');
       return 3;
     }
   }
