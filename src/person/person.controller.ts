564b9c4c5ca98391ebeadecef6c2c926859fef70
rosset25 <al341849@uji.es>
2020-04-27 21:49:19 +0200

    corregido error

--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -11,14 +11,11 @@ import {
 } from '@nestjs/common';
 import { PersonDto } from '../person/person.dto';
 import { PersonService } from '../person/person.service';
-<<<<<<< HEAD
 import { SubscriptionService } from 'src/subscription/subscription.service';
-=======
 import { AuthGuard } from '../guards/auth.guard';
 import { AuthUser } from '../shared/decorators/requester.decorator';
 import { Requester } from '../shared/entities/requester';
 import { RequesterRole } from '../shared/enums/requester-role.enum';
->>>>>>> 4eecda61dd9c7e570556cfdd5702bca3239e3158
 
 @Controller('person')
 export class PersonController {
@@ -30,7 +27,7 @@ export class PersonController {
   @Get()
   @UseGuards(AuthGuard)
   async getPeople(@AuthUser() requester: Requester) {
-    if(requester.role !== "PERSON"){
+    if (requester.role !== 'PERSON') {
       throw new ForbiddenException();
     }
     return await this.personService.getPeople();
@@ -39,9 +36,11 @@ export class PersonController {
   @Get(':id')
   @UseGuards(AuthGuard)
   async getPerson(@AuthUser() requester: Requester, @Param('id') id: string) {
-
-    if(requester.role !== RequesterRole.PERSON || parseInt(id) !== requester.id){
-      console.log("Recurso")
+    if (
+      requester.role !== RequesterRole.PERSON ||
+      parseInt(id) !== requester.id
+    ) {
+      console.log('Recurso');
 
       throw new ForbiddenException();
     }
@@ -55,34 +54,38 @@ export class PersonController {
   }
 
   @Put(':id')
-<<<<<<< HEAD
+  @UseGuards(AuthGuard)
   async updatePerson(
-    @Param('id') id: number,
+    @AuthUser() requester: Requester,
+    @Param('id') id: string,
     @Body() personAndSubscriptions: any,
   ) {
+    if (
+      requester.role !== RequesterRole.PERSON ||
+      parseInt(id) !== requester.id
+    ) {
+      throw new ForbiddenException();
+    }
+
     let personDto: PersonDto = personAndSubscriptions.person;
     let subscriptions: string[] = personAndSubscriptions.subscriptions.subs; //Array.prototype.slice.call(personAndSubscriptions.subscriptions,);
 
-    personDto.id = id;
+    personDto.id = parseInt(id);
 
     this.subscriptionService.updateSubscriptions(personDto.id, subscriptions);
-=======
-  @UseGuards(AuthGuard)
-  async updatePerson(@AuthUser() requester: Requester, @Param('id') id: string, @Body() personDto: PersonDto) {
-   
-    
-    if(requester.role !== RequesterRole.PERSON || parseInt(id) !== requester.id){
-      throw new ForbiddenException();
-    }
-    personDto.id =  parseInt(id);
->>>>>>> 4eecda61dd9c7e570556cfdd5702bca3239e3158
     return this.personService.updatePerson(personDto);
   }
 
   @Delete(':id')
   @UseGuards(AuthGuard)
-  async deletePerson(@AuthUser() requester: Requester, @Param('id') id: string) {
-    if(requester.role !== RequesterRole.PERSON || parseInt(id) !== requester.id){
+  async deletePerson(
+    @AuthUser() requester: Requester,
+    @Param('id') id: string,
+  ) {
+    if (
+      requester.role !== RequesterRole.PERSON ||
+      parseInt(id) !== requester.id
+    ) {
       throw new ForbiddenException();
     }
     return this.personService.deletePerson(parseInt(id));
